6d138d1a9ae15ebcfb93ab92dcad7f7fece68daa
Islam Hamdi <archtesla.engineer@gmail.com>
2025-12-18 14:55:59 +0200

    Create say_hello_world.py

---
+++ introduction/say_hello_world.py
@@ -0,0 +1,7 @@
+"""
+Problem: Say "Hello, World!" With Python
+Link: https://www.hackerrank.com/challenges/py-hello-world/problem
+Approach: Basic print statement to output the required string.
+"""
+
+print("Hello, World!")
